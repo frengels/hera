890b8941c5c83a059fd9f8b5a19fb18da0756d6c
Frederik Engels <frederik.engels92@gmail.com>
2019-08-21 12:02:39 +0200

    test that hera::get properly performs sfinae on out of bounds operations

--- include/hera/get.hpp
+++ include/hera/get.hpp
@@ -10,19 +10,27 @@ namespace get_impl
 {
 using hera::detail::priority_tag;
 
-template<std::size_t I, typename T>
-constexpr auto
+template<std::size_t I, typename T> // clang-format off
+    requires (I < std::tuple_size_v<std::remove_cvref_t<T>>) &&
+        requires(T&& t)
+        {
+            std::forward<T>(t).template get<I>();
+        }
+constexpr decltype(auto) // clang-format on
     get(priority_tag<4>,
         T&& t) noexcept(noexcept(std::forward<T>(t).template get<I>()))
-        -> decltype(std::forward<T>(t).template get<I>())
 {
     return std::forward<T>(t).template get<I>();
 }
 
-template<std::size_t I, typename T>
-constexpr auto get(priority_tag<3>,
-                   T&& t) noexcept(noexcept(get<I>(std::forward<T>(t))))
-    -> decltype(get<I>(std::forward<T>(t)))
+template<std::size_t I, typename T> // clang-format off
+    requires (I < std::tuple_size_v<std::remove_cvref_t<T>>) &&
+        requires(T&& t)
+        {
+            get<I>(std::forward<T>(t));
+        } // clang-format on
+constexpr decltype(auto)
+    get(priority_tag<3>, T&& t) noexcept(noexcept(get<I>(std::forward<T>(t))))
 {
     return get<I>(std::forward<T>(t));
 }
@@ -46,13 +54,12 @@ template<std::size_t I>
 struct get_fn
 {
     template<typename T> // clang-format off
-        requires 
-            (I < std::tuple_size_v<std::remove_cvref_t<T>>) &&
-            impl::has_get_impl<T, I>
-    constexpr decltype(auto) // clang-format on
-        operator()(T&& t) const
+    constexpr auto // clang-format on
+    operator()(T&& t) const
         noexcept(noexcept(hera::get_impl::get<I>(detail::max_priority_tag,
                                                  std::forward<T>(t))))
+            -> decltype(hera::get_impl::get<I>(detail::max_priority_tag,
+                                               std::forward<T>(t)))
     {
         return hera::get_impl::get<I>(detail::max_priority_tag,
                                       std::forward<T>(t));
