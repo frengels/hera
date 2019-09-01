4096dcb817b0f293a28f86cd2cb0c42398fc3eba
Frederik Engels <frederik.engels92@gmail.com>
2019-09-01 16:53:43 +0200

    add any_of and all_of algorithms to reflect on traits

---
+++ include/hera/algorithm/any_of.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "hera/begin_end.hpp"
+#include "hera/iterator.hpp"
+#include "hera/metafunction.hpp"
+#include "hera/next_prev.hpp"
+#include "hera/ranges.hpp"
+#include "hera/type_identity.hpp"
+
+namespace hera
+{
+struct any_of_fn
+{
+    template<bool                   B,
+             hera::forward_iterator I,
+             hera::sentinel_for<I>  S,
+             hera::metafunction PredMeta> // clang-format off
+    static constexpr auto invoke(I first, S last, PredMeta pm) noexcept // clang-format on
+    {
+        if constexpr (decltype(first == last)::value)
+        {
+            return std::bool_constant<B>{};
+        }
+        else
+        {
+            using invoke_res = std::invoke_result_t<typename PredMeta::type,
+                                                    iter_reference_t<I>>;
+            return invoke<(B || invoke_res::value)>(
+                hera::next(first), std::move(last), std::move(pm));
+        }
+    }
+
+    template<hera::forward_iterator I,
+             hera::sentinel_for<I>  S,
+             hera::metafunction     PredMeta>
+    constexpr auto operator()(I first, S last, PredMeta pm) const noexcept
+        -> decltype(invoke<false>(std::move(first),
+                                  std::move(last),
+                                  std::move(pm)))
+    {
+        return invoke<false>(std::move(first), std::move(last), std::move(pm));
+    }
+
+    template<hera::forward_iterator I, hera::sentinel_for<I> S, typename Pred>
+    constexpr auto operator()(I first, S last, Pred) const noexcept
+        -> decltype(invoke<false>(std::move(first),
+                                  std::move(last),
+                                  hera::type_identity<Pred>{}))
+    {
+        return invoke<false>(
+            std::move(first), std::move(last), hera::type_identity<Pred>{});
+    }
+
+    template<hera::forward_iterator I,
+             hera::sentinel_for<I>  S,
+             hera::metafunction     PredMeta>
+    constexpr auto operator()(I first, S last, PredMeta pm) const noexcept
+        -> decltype(invoke<false>(std::move(first), std::move(last)))
+    {
+        return invoke<false>(std::move(first), std::move(last), std::move(pm));
+    }
+
+    template<hera::forward_range R, typename Pred>
+    constexpr auto operator()(R&& r, Pred) const noexcept
+        -> decltype(invoke<false>(hera::begin(r),
+                                  hera::end(r),
+                                  hera::type_identity<Pred>{}))
+    {
+        return invoke<false>(
+            hera::begin(r), hera::end(r), hera::type_identity<Pred>{});
+    }
+
+    template<hera::forward_range R, hera::metafunction PredMeta>
+    constexpr auto operator()(R&& r, PredMeta pm) const noexcept
+        -> decltype(invoke<false>(hera::begin(r), hera::end(r), std::move(pm)))
+    {
+        return invoke<false>(hera::begin(r), hera::end(r), std::move(pm));
+    }
+};
+
+constexpr auto any_of = any_of_fn{};
+} // namespace hera
---
+++ tests/any_of_all_of.cpp
@@ -0,0 +1,61 @@
+#include <catch2/catch.hpp>
+
+#include "hera/algorithm/all_of.hpp"
+#include "hera/algorithm/any_of.hpp"
+#include "hera/container/type_list.hpp"
+
+TEST_CASE("any_of")
+{
+    // these are all empty
+    auto empty_tl =
+        hera::type_list<hera::type_identity<int>, hera::type_identity<float>>{};
+    auto nonempty_tl = hera::type_list<int, float>{}; // none of these are empty
+    auto half_tl =
+        hera::type_list<int, hera::type_identity<float>>{}; // one is empty
+
+    static_assert(!decltype(hera::any_of(empty_tl, [](auto ident) {
+        return std::bool_constant<
+            !std::is_empty_v<typename decltype(ident)::type>>{};
+    }))::value);
+
+    static_assert(decltype(hera::any_of(nonempty_tl, [](auto ident) {
+        return std::bool_constant<
+            !std::is_empty_v<typename decltype(ident)::type>>{};
+    }))::value);
+
+    static_assert(decltype(hera::any_of(half_tl, [](auto ident) {
+        return std::is_empty<typename decltype(ident)::type>{};
+    }))::value);
+
+    static_assert(decltype(hera::any_of(half_tl, [](auto ident) {
+        return std::bool_constant<
+            !std::is_empty_v<typename decltype(ident)::type>>{};
+    }))::value);
+}
+
+TEST_CASE("all_of")
+{
+    // these are all empty
+    auto empty_tl =
+        hera::type_list<hera::type_identity<int>, hera::type_identity<float>>{};
+    auto nonempty_tl = hera::type_list<int, float>{}; // none of these are empty
+    auto half_tl =
+        hera::type_list<int, hera::type_identity<float>>{}; // one is empty
+
+    static_assert(decltype(hera::all_of(empty_tl, [](auto ident) {
+        return std::is_empty<typename decltype(ident)::type>{};
+    }))::value);
+
+    static_assert(!decltype(hera::all_of(nonempty_tl, [](auto ident) {
+        return std::is_empty<typename decltype(ident)::type>{};
+    }))::value);
+
+    static_assert(!decltype(hera::all_of(half_tl, [](auto ident) {
+        return std::is_empty<typename decltype(ident)::type>{};
+    }))::value);
+
+    static_assert(!decltype(hera::all_of(half_tl, [](auto ident) {
+        return std::bool_constant<
+            !std::is_empty_v<typename decltype(ident)::type>>{};
+    }))::value);
+}
