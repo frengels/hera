8247f14b6a30738c9c64fdbb3cbbbc0b427422fd
Frederik Engels <frederik.engels92@gmail.com>
2019-10-15 20:58:49 +0200

    move begin cpo into fn struct

--- include/hera/begin_end.hpp
+++ include/hera/begin_end.hpp
@@ -11,58 +11,46 @@ namespace hera
 {
 namespace begin_impl
 {
-using hera::detail::decay_copy;
-using hera::detail::priority_tag;
-
-// constraint is satisfied if the passed type is a heterogeneous iterator
-template<typename I> // clang-format off
-    requires hera::heterogeneous_iterator<std::remove_cvref_t<I>>
-constexpr decltype(auto) // clang-format on
-cond_het(I&& i) noexcept(noexcept(decay_copy(std::forward<I>(i))))
-{
-    return decay_copy(std::forward<I>(i));
-}
-
-template<typename R>
-constexpr auto begin(priority_tag<4>,
-                     R& r) noexcept(noexcept(cond_het(r.begin())))
-    -> decltype(cond_het(r.begin()))
-{
-    return cond_het(r.begin());
-}
-
 template<typename R>
 void begin(R&&) = delete;
 
-template<typename R>
-constexpr auto
-    begin(priority_tag<3>,
-          R&& r) noexcept(noexcept(cond_het(begin(std::forward<R>(r)))))
-        -> decltype(cond_het(begin(std::forward<R>(r))))
+struct fn
 {
-    return cond_het(begin(std::forward<R>(r)));
-}
+private:
+    template<typename R>
+    static constexpr auto impl(hera::detail::priority_tag<4>,
+                               R& r) noexcept(noexcept(r.begin()))
+        -> decltype(r.begin())
+    {
+        return r.begin();
+    }
+
+    template<typename R>
+    static constexpr auto
+    impl(hera::detail::priority_tag<3>,
+         R&& r) noexcept(noexcept(begin(std::forward<R>(r))))
+        -> decltype(begin(std::forward<R>(r)))
+    {
+        return begin(std::forward<R>(r));
+    }
+
+public:
+    template<typename R>
+    constexpr auto operator()(R&& r) const
+        noexcept(noexcept(decay_copy(impl(hera::detail::max_priority_tag,
+                                          std::forward<R>(r)))))
+            -> decltype(decay_copy(impl(hera::detail::max_priority_tag,
+                                        std::forward<R>(r))))
+    {
+        return decay_copy(
+            impl(hera::detail::max_priority_tag, std::forward<R>(r)));
+    }
+};
 } // namespace begin_impl
 
 inline namespace cpo
 {
-// begin on a zero sized range will not be valid for the heterogeneous iterator,
-// find a solution for that.
-struct begin_fn
-{
-    template<typename R>
-    constexpr auto operator()(R&& r) const noexcept(
-        noexcept(::hera::begin_impl::begin(hera::detail::max_priority_tag,
-                                           std::forward<R>(r))))
-        -> decltype(::hera::begin_impl::begin(hera::detail::max_priority_tag,
-                                              std::forward<R>(r)))
-    {
-        return ::hera::begin_impl::begin(hera::detail::max_priority_tag,
-                                         std::forward<R>(r));
-    }
-};
-
-constexpr auto begin = begin_fn{};
+inline constexpr auto begin = begin_impl::fn{};
 } // namespace cpo
 
 namespace end_impl
@@ -72,9 +60,9 @@ using hera::detail::priority_tag;
 
 template<typename R>
 constexpr auto
-    end(priority_tag<4>,
-        R& r) noexcept(noexcept(decay_copy(std::forward<R>(r).end())))
-        -> decltype(decay_copy(std::forward<R>(r).end()))
+end(priority_tag<4>,
+    R& r) noexcept(noexcept(decay_copy(std::forward<R>(r).end())))
+    -> decltype(decay_copy(std::forward<R>(r).end()))
 {
     return decay_copy(std::forward<R>(r).end());
 }
@@ -84,9 +72,9 @@ void end(R&&) = delete;
 
 template<typename R>
 constexpr auto
-    end(priority_tag<3>,
-        R&& r) noexcept(noexcept(decay_copy(end(std::forward<R>(r)))))
-        -> decltype(decay_copy(end(std::forward<R>(r))))
+end(priority_tag<3>,
+    R&& r) noexcept(noexcept(decay_copy(end(std::forward<R>(r)))))
+    -> decltype(decay_copy(end(std::forward<R>(r))))
 {
     return decay_copy(end(std::forward<R>(r)));
 }
