026e89b574a3a704a09abdfab2abb7e0d99e7224
Frederik Engels <frederik.engels92@gmail.com>
2020-01-18 13:54:35 +0100

    adjust iota to work without iterator

--- include/hera/view/iota.hpp
+++ include/hera/view/iota.hpp
@@ -1,123 +1,64 @@
 #pragma once
 
-#include "hera/constant.hpp"
-#include "hera/iterator/concepts.hpp"
-#include "hera/iterator/indexed_interface.hpp"
-#include "hera/iterator/sentinel.hpp"
+#include "hera/bound.hpp"
+#include "hera/ranges.hpp"
 #include "hera/view/interface.hpp"
 
 namespace hera
 {
-template<auto W,
-         auto Bound     = static_cast<decltype(W)>(0),
-         bool Unbounded = true> // clang-format off
-    requires same_as<decltype(W), decltype(Bound)>
-class iota_view : public view_interface<iota_view<W, Bound, Unbounded>> { // clang-format on
-private:
-    template<decltype(W) I>
-    class iterator : public hera::indexed_interface<
-                         iterator<I>,
-                         std::ptrdiff_t,
-                         I,
-                         std::conditional_t<Unbounded,
-                                            hera::unbounded_extent,
-                                            hera::bounded_extent<Bound>>> {
-    public:
-        using value_type      = std::integral_constant<decltype(I), I>;
-        using difference_type = std::ptrdiff_t;
-
-    public:
-        constexpr iterator() noexcept = default;
-
-        constexpr auto operator++() const noexcept
-        {
-            return iterator<I + 1>{};
-        }
-
-        constexpr auto operator--() const noexcept
-        {
-            return iterator<I - 1>{};
-        }
-
-        template<hera::constant_convertible_to<difference_type> C>
-        constexpr auto operator+(C) const noexcept
-        {
-            return iterator<I + C::value>{};
-        }
-
-        template<hera::constant_convertible_to<difference_type> C>
-        friend constexpr auto operator+(C, const iterator&) noexcept
-        {
-            return iterator<C::value + I>{};
-        }
-
-        template<hera::constant_convertible_to<difference_type> C>
-        constexpr auto operator-(C) const noexcept
-        {
-            return iterator<I - C::value>{};
-        }
-
-        template<auto X>
-        constexpr auto operator-(const iterator&) const noexcept
-        {
-            return iterator<I - X>{};
-        }
-
-        constexpr decltype(auto) operator*() const noexcept
-        {
-            return std::integral_constant<decltype(I), I>{};
-        }
-
-        template<hera::constant_convertible_to<difference_type> C>
-        constexpr decltype(auto) operator[](C n) const noexcept
-        {
-            return *((*this) + n);
-        }
-    };
-
+template<auto Start, auto Bound = hera::infinite{}>
+class iota_view : public hera::view_interface<iota_view<Start, Bound>>
+{
 public:
-    constexpr iota_view() noexcept = default;
+    using value_type = decltype(Start);
 
-    template<hera::constant C>
-    constexpr iota_view(C) noexcept
-    {}
+private:
+    static_assert(
+        []() -> bool {
+            if constexpr (hera::unbounded<std::remove_cvref_t<decltype(Bound)>>)
+            {
+                return true;
+            }
+            else
+            {
+                return Bound >= Start;
+            }
+        }(),
+        "Bound smaller than Start");
 
-    template<hera::constant                                 C,
-             hera::constant_same_as<typename C::value_type> End>
-    constexpr iota_view(C, End) noexcept
-    {}
-
-    constexpr auto begin() const noexcept
-    {
-        return iterator<W>{};
-    }
-
-    constexpr auto end() const noexcept
-    {
-        if constexpr (Unbounded)
+    template<std::size_t I>
+    static constexpr bool in_range = []() -> bool {
+        if constexpr (hera::unbounded<std::remove_cvref_t<decltype(Bound)>>)
         {
-            return hera::unbounded_sentinel{};
+            return true;
         }
         else
         {
-            return hera::bounded_sentinel{};
+            return I < (Bound - Start);
+        }
+    }();
+
+public:
+    iota_view() = default;
+
+    constexpr auto size() const noexcept
+    {
+        if constexpr (hera::unbounded<std::remove_cvref_t<decltype(Bound)>>)
+        {
+            return hera::infinite{};
+        }
+        else
+        {
+            return std::integral_constant<std::make_unsigned_t<decltype(Bound)>,
+                                          Bound - Start>{};
         }
     }
 
-    friend constexpr auto begin(iota_view iv) noexcept(noexcept(iv.begin()))
+    template<std::size_t I> // clang-format off
+        requires in_range<I>
+    constexpr auto at() const noexcept // clang-format on
     {
-        return iv.begin();
-    }
-
-    friend constexpr auto end(iota_view iv) noexcept(noexcept(iv.end()))
-    {
-        return iv.end();
+        return std::integral_constant<value_type, Start + I>{};
     }
 };
-
-template<hera::constant C>
-iota_view(C)->iota_view<C::value>;
-
-template<hera::constant C, hera::constant_same_as<typename C::value_type> Bound>
-iota_view(C, Bound)->iota_view<C::value, Bound::value, false>;
 } // namespace hera
--- tests/iota_view.cpp
+++ tests/iota_view.cpp
@@ -8,16 +8,20 @@ constexpr auto verify_unbounded_range(R)
 
 TEST_CASE("iota_view")
 {
-    auto iota = hera::iota_view<0>{};
+    SECTION("finite")
+    {
+        auto iota = hera::iota_view<0>{};
 
-    verify_unbounded_range(iota);
-    static_assert(!hera::bounded_range<decltype(iota)>);
-    static_assert(hera::forwarding_range<decltype(iota)>);
+        verify_unbounded_range(iota);
+        static_assert(!hera::bounded_range<decltype(iota)>);
+        REQUIRE(hera::at<50>(iota) == 50);
+    }
 
-    auto beg = iota.begin();
+    SECTION("finite")
+    {
+        auto iota = hera::iota_view<10, 50>{};
 
-    REQUIRE(*beg == 0);
-
-    REQUIRE(beg[std::integral_constant<std::ptrdiff_t, 50>{}] == 50);
-    REQUIRE(*++beg == 1);
+        static_assert(hera::bounded_range<decltype(iota)>);
+        REQUIRE(hera::at<10>(iota) == 20);
+    }
 }
