a0a47a480cee1606506d0a5fddc5dc6e63b18565
Frederik Engels <frederik.engels92@gmail.com>
2020-01-18 12:21:06 +0100

    modify view concepts to work without iterators

--- include/hera/ranges.hpp
+++ include/hera/ranges.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "hera/at.hpp"
 #include "hera/size.hpp"
 
 namespace hera
@@ -19,4 +20,15 @@ concept empty_range = bounded_range<R>&& // clang-format off
     {
         requires decltype(hera::size(r))::value == 0;
     }; // clang-format on
+
+template<hera::range T>
+inline constexpr bool enable_safe_range = false;
+
+template<typename T>
+concept safe_range = range<T> && (std::is_lvalue_reference_v<T> ||
+                                  enable_safe_range<std::remove_cvref_t<T>>);
+
+template<hera::range R, std::size_t I> // clang-format off
+    requires !hera::empty_range<R> // clang-format on
+    using range_reference_at_t = decltype(hera::at<I>(std::declval<R&>()));
 } // namespace hera
--- include/hera/view.hpp
+++ include/hera/view.hpp
@@ -10,8 +10,9 @@ namespace detail
 {
 template<typename T>
 concept container_like =
-    hera::range<T>&& hera::range<const T> &&
-    !same_as<range_reference_t<T>, range_reference_t<const T>>;
+    hera::range<T>&& hera::range<const T> && !hera::empty_range<T> &&
+    !same_as<hera::range_reference_at_t<T, 0>,
+             hera::range_reference_at_t<const T, 0>>;
 
 template<typename T>
 concept enable_view_default =
@@ -25,6 +26,5 @@ template<typename T>
 concept view = range<T>&& enable_view<T>;
 
 template<typename R>
-concept viewable_range = range<R> &&
-                         (forwarding_range<R> || view<std::decay_t<R>>);
+concept viewable_range = range<R> && (safe_range<R> || view<std::decay_t<R>>);
 } // namespace hera
