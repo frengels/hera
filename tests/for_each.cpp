0deab6c8bf596d45c5b791b4f76adc665b4cd422
Frederik Engels <frederik.engels92@gmail.com>
2020-01-18 15:27:00 +0100

    for_each now uses unpack

--- tests/for_each.cpp
+++ tests/for_each.cpp
@@ -1,27 +1,18 @@
 #include <catch2/catch.hpp>
 
-#include "hera/view/filter.hpp"
-#include "hera/view/tuple.hpp"
+#include "hera/view/tuplelike.hpp"
 
 #include "hera/algorithm/for_each.hpp"
 
 TEST_CASE("for_each")
 {
-    auto tup = std::make_tuple("hi", "bye", 1, 2, 3, "again", 4, 5);
-
-    auto filtered =
-        hera::views::tuple(tup) | hera::views::filter([](auto x) {
-            return std::bool_constant<hera::same_as<int, decltype(x)>>{};
-        });
+    auto tup = std::make_tuple("hi", "bye", "again");
 
     // iterator based
-    hera::for_each(hera::begin(filtered),
-                   hera::end(filtered),
-                   [i = 1](auto x) mutable { REQUIRE(x == i++); });
-
-    // range based
-    hera::for_each(filtered, [i = 1](auto x) mutable { REQUIRE(x == i++); });
-
-    static_assert(noexcept(hera::for_each(
-        filtered, [i = 1](auto x) mutable noexcept { REQUIRE(x == i++); })));
+    auto last_len =
+        hera::for_each(hera::tuplelike_view(tup), [len = 0](auto x) mutable {
+            len = std::string_view(x).size();
+            return len;
+        });
+    REQUIRE(last_len == 5);
 }
