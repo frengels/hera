7360177c1dd2299fe753b3e3ffd3b6ef98703504
Frederik Engels <frederik.engels92@gmail.com>
2020-01-19 20:00:08 +0100

    add equality checks to self and other constants

--- include/hera/bound.hpp
+++ include/hera/bound.hpp
@@ -10,7 +10,43 @@ namespace hera
 {
 // a special marker type to indicate the size is infinite
 struct infinite
-{};
+{
+    constexpr std::true_type operator==(const infinite&) const noexcept
+    {
+        return {};
+    }
+
+    constexpr std::false_type operator!=(const infinite&) const noexcept
+    {
+        return {};
+    }
+
+    template<hera::constant_convertible_to<std::size_t> C>
+    constexpr std::false_type operator==(const C&) const noexcept
+    {
+        return {};
+    }
+
+    template<hera::constant_convertible_to<std::size_t> C>
+    constexpr std::true_type operator!=(const C&) const noexcept
+    {
+        return {};
+    }
+
+    template<hera::constant_convertible_to<std::size_t> C>
+    friend constexpr std::false_type operator==(const C&,
+                                                const infinite&) noexcept
+    {
+        return {};
+    }
+
+    template<hera::constant_convertible_to<std::size_t> C>
+    friend constexpr std::true_type operator!=(const C&,
+                                               const infinite&) noexcept
+    {
+        return {};
+    }
+};
 
 // bounded ranges are any integral constant convertible to size_t
 template<typename B>
