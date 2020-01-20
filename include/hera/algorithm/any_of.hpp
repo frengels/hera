b3916e14616aeb648654f521d1d2f6178796e4a6
Frederik Engels <frederik.engels92@gmail.com>
2020-01-20 05:29:30 +0100

    any_of written with just some clever transforms

--- include/hera/algorithm/any_of.hpp
+++ include/hera/algorithm/any_of.hpp
@@ -1,82 +1,27 @@
 #pragma once
 
-#include "hera/begin_end.hpp"
-#include "hera/iterator/concepts.hpp"
-#include "hera/metafunction.hpp"
-#include "hera/next_prev.hpp"
+#include "hera/algorithm/find_if.hpp"
 #include "hera/ranges.hpp"
 #include "hera/type_identity.hpp"
 
 namespace hera
 {
-struct any_of_fn
+namespace any_of_impl
 {
-    template<bool                   B,
-             hera::forward_iterator I,
-             hera::sentinel_for<I>  S,
-             hera::metafunction PredMeta> // clang-format off
-    static constexpr auto invoke(I first, S last, PredMeta pm) noexcept // clang-format on
+struct fn
+{
+public:
+    template<hera::range R, typename Pred>
+    constexpr auto operator()(R&& range, Pred p) const noexcept
     {
-        if constexpr (decltype(first == last)::value)
-        {
-            return std::bool_constant<B>{};
-        }
-        else
-        {
-            using invoke_res = std::invoke_result_t<typename PredMeta::type,
-                                                    iter_reference_t<I>>;
-            return invoke<(B || invoke_res::value)>(
-                hera::next(first), std::move(last), std::move(pm));
-        }
-    }
+        // this will find the first occurence
+        auto res = hera::find_if(std::forward<R>(range), std::move(p));
 
-    template<hera::forward_iterator I,
-             hera::sentinel_for<I>  S,
-             hera::metafunction     PredMeta>
-    constexpr auto operator()(I first, S last, PredMeta pm) const noexcept
-        -> decltype(invoke<false>(std::move(first),
-                                  std::move(last),
-                                  std::move(pm)))
-    {
-        return invoke<false>(std::move(first), std::move(last), std::move(pm));
-    }
-
-    template<hera::forward_iterator I, hera::sentinel_for<I> S, typename Pred>
-    constexpr auto operator()(I first, S last, Pred) const noexcept
-        -> decltype(invoke<false>(std::move(first),
-                                  std::move(last),
-                                  hera::type_identity<Pred>{}))
-    {
-        return invoke<false>(
-            std::move(first), std::move(last), hera::type_identity<Pred>{});
-    }
-
-    template<hera::forward_iterator I,
-             hera::sentinel_for<I>  S,
-             hera::metafunction     PredMeta>
-    constexpr auto operator()(I first, S last, PredMeta pm) const noexcept
-        -> decltype(invoke<false>(std::move(first), std::move(last)))
-    {
-        return invoke<false>(std::move(first), std::move(last), std::move(pm));
-    }
-
-    template<hera::forward_range R, typename Pred>
-    constexpr auto operator()(R&& r, Pred) const noexcept
-        -> decltype(invoke<false>(hera::begin(r),
-                                  hera::end(r),
-                                  hera::type_identity<Pred>{}))
-    {
-        return invoke<false>(
-            hera::begin(r), hera::end(r), hera::type_identity<Pred>{});
-    }
-
-    template<hera::forward_range R, hera::metafunction PredMeta>
-    constexpr auto operator()(R&& r, PredMeta pm) const noexcept
-        -> decltype(invoke<false>(hera::begin(r), hera::end(r), std::move(pm)))
-    {
-        return invoke<false>(hera::begin(r), hera::end(r), std::move(pm));
+        return res.transform([](auto&&) { return std::true_type{}; })
+            .value_or(std::false_type{});
     }
 };
+} // namespace any_of_impl
 
-constexpr auto any_of = any_of_fn{};
+inline constexpr auto any_of = any_of_impl::fn{};
 } // namespace hera
