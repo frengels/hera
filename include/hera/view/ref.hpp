73e439b8a54e54633c39f3216e3104cf5b548302
Frederik Engels <frederik.engels92@gmail.com>
2020-01-20 04:55:48 +0100

    remove at and add try_at for ref_view

--- include/hera/view/ref.hpp
+++ include/hera/view/ref.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "hera/begin_end.hpp"
-#include "hera/empty.hpp"
 #include "hera/ranges.hpp"
 #include "hera/size.hpp"
 #include "hera/view/interface.hpp"
@@ -42,10 +40,9 @@ public:
     }
 
     template<std::size_t I>
-    constexpr auto at() const noexcept(noexcept(hera::at<I>(*base_)))
-        -> decltype(hera::at<I>(*base_))
+    constexpr auto try_at() const noexcept
     {
-        return hera::at<I>(*base_);
+        return hera::try_at<I>(*base_);
     }
 };
 
@@ -54,60 +51,4 @@ ref_view(R&)->ref_view<R>;
 
 template<typename T>
 inline constexpr bool enable_safe_range<ref_view<T>> = true;
-
-/*
-template<hera::range R>
-requires std::is_object_v<R> class ref_view : public view_interface<ref_view<R>>
-{
-private:
-    R& base_;
-
-public:
-    template<typename D = R> // clang-format off
-        requires !same_as<D, ref_view> // clang-format on
-        constexpr ref_view(R & range) noexcept : base_{range}
-    {}
-
-    constexpr R& base() const noexcept
-    {
-        return base_;
-    }
-
-    constexpr auto begin() const noexcept(noexcept(hera::begin(base())))
-    {
-        return hera::begin(base());
-    }
-
-    constexpr auto end() const noexcept(noexcept(hera::end(base())))
-    {
-        return hera::end(base());
-    }
-
-    template<typename D = R> // clang-format off
-        requires requires(D& d)
-        {
-            hera::empty(d);
-        } // clang-format on
-    constexpr auto empty() const noexcept(noexcept(hera::empty(base())))
-    {
-        return hera::empty(base());
-    }
-
-    template<hera::sized_range D = R>
-    constexpr auto size() const noexcept(noexcept(hera::size(base())))
-    {
-        return hera::size(base());
-    }
-
-    friend constexpr auto begin(ref_view r) noexcept(noexcept(r.begin()))
-    {
-        return r.begin();
-    }
-
-    friend constexpr auto end(ref_view r) noexcept(noexcept(r.end()))
-    {
-        return r.end();
-    }
-};
-*/
 } // namespace hera
