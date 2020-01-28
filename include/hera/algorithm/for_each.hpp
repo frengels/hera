1f8ed48a13db18b6500a3dacdde970f4910ca48b
Frederik Engels <frederik.engels92@gmail.com>
2020-01-28 16:52:51 +0100

    remove forward calls from for_each

--- include/hera/algorithm/for_each.hpp
+++ include/hera/algorithm/for_each.hpp
@@ -13,8 +13,8 @@ public:
     template<hera::range R, typename F>
     constexpr decltype(auto) operator()(R&& range, F fn) const
     {
-        return hera::unpack(std::forward<R>(range), [&](auto&&... xs) {
-            return (fn(std::forward<decltype(xs)>(xs)), ...);
+        return hera::unpack(static_cast<R&&>(range), [&](auto&&... xs) {
+            return (fn(static_cast<decltype(xs)&&>(xs)), ...);
         });
     }
 };
