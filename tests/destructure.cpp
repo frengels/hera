105658c3015d0265a2c1efea2dc50869d682e36c
Frederik Engels <frederik.engels92@gmail.com>
2020-01-28 23:07:23 +0100

    add initial test for destructuring, doesn't pass due to compiler bugs

---
+++ tests/destructure.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch.hpp>
+
+#include "hera/container/tuple.hpp"
+#include "hera/destructure.hpp"
+
+TEST_CASE("destructure")
+{
+    auto tup = hera::make_tuple(5.0, 4.0f, 1, "hello");
+
+    auto& [d, f, i, str] = tup;
+}
