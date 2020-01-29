38b8c0a2bbfb835e9d7087f0516fc4dd9de6c492
Frederik Engels <frederik.engels92@gmail.com>
2020-01-29 02:44:10 +0100

    make it possible to change the type of the accumulated object through recursion

--- include/hera/algorithm/accumulate.hpp
+++ include/hera/algorithm/accumulate.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <utility>
-
-#include "hera/algorithm/unpack.hpp"
 #include "hera/ranges.hpp"
 
 namespace hera
@@ -11,29 +8,53 @@ namespace accumulate_impl
 {
 struct fn
 {
+    template<std::size_t         I,
+             hera::bounded_range R,
+             typename T,
+             typename BinaryOp> // clang-format off
+        requires (I < hera::size_v<R>)
+    static constexpr decltype(auto) recurse(R&& range, T&& accu, BinaryOp&& op) // clang-format on
+    {
+        static_assert(std::is_invocable_v<BinaryOp,
+                                          T,
+                                          decltype(hera::get<I>(
+                                              static_cast<R&&>(range)))>,
+                      "Cannot invoke BinaryOp on T and get<I>(range)");
+
+        return recurse<I + 1>(
+            static_cast<R&&>(range),
+            op(static_cast<T&&>(accu), hera::get<I>(static_cast<R&&>(range))),
+            static_cast<BinaryOp&&>(op));
+    }
+
+    template<std::size_t         I,
+             hera::bounded_range R,
+             typename T,
+             typename BinaryOp> // clang-format off
+        requires (I == hera::size_v<R>)
+    static constexpr auto recurse(R&& range, T&& final_res, BinaryOp&&) // clang-format on
+    {
+        return static_cast<T&&>(final_res);
+    }
+
 public:
     template<hera::bounded_range R, typename T>
-    constexpr T operator()(R&& range, T init) const
+    constexpr auto operator()(R&& range, T init) const
     {
-        auto add_to_init = [&](auto&& x) { init = std::move(init) + x; };
-
-        hera::unpack(static_cast<R&&>(range), [&](auto&&... xs) {
-            (add_to_init(static_cast<decltype(xs)&&>(xs)), ...);
-        });
-
-        return init;
+        return recurse<0>(static_cast<R&&>(range),
+                          static_cast<T&&>(init),
+                          [](auto&& accu, auto&& curr) {
+                              return static_cast<decltype(accu)&&>(accu) +
+                                     static_cast<decltype(curr)&&>(curr);
+                          });
     }
 
     template<hera::bounded_range R, typename T, typename BinaryOp>
     constexpr T operator()(R&& range, T init, BinaryOp&& op) const
     {
-        auto add_to_init = [&](auto&& x) { init = op(std::move(init), x); };
-
-        hera::unpack(static_cast<R&&>(range), [&](auto&&... xs) {
-            (add_to_init(static_cast<decltype(xs)&&>(xs)), ...);
-        });
-
-        return init;
+        return recurse<0>(static_cast<R&&>(range),
+                          static_cast<T&&>(init),
+                          static_cast<BinaryOp&&>(op));
     }
 };
 } // namespace accumulate_impl
