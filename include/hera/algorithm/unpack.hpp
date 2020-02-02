7b8358298cc8d4831f3cc2dc171800f5cafd70bf
Frederik Engels <frederik.engels92@gmail.com>
2020-02-02 15:30:03 +0100

    use size_v to get size and guarantee constant expression

--- include/hera/algorithm/unpack.hpp
+++ include/hera/algorithm/unpack.hpp
@@ -24,7 +24,7 @@ public:
     template<hera::bounded_range R, typename F>
     constexpr decltype(auto) operator()(R&& r, F&& fn) const noexcept
     {
-        constexpr std::size_t sz  = hera::size(r);
+        constexpr std::size_t sz  = hera::size_v<R>;
         constexpr auto        seq = std::make_index_sequence<sz>{};
         return impl(seq, static_cast<R&&>(r), static_cast<F&&>(fn));
     }
