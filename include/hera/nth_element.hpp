cfa26b3e3fe39672ae4ff76a2e7d106862041595
Frederik Engels <frederik.engels92@gmail.com>
2020-03-02 17:06:20 +0100

    add nth_element

---
+++ include/hera/nth_element.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "hera/container/integer_sequence.hpp"
+
+namespace hera
+{
+namespace detail
+{
+template<std::size_t, typename T>
+struct indexed
+{
+    using type = T;
+};
+
+template<typename Is, typename... Ts>
+struct indexer;
+
+template<std::size_t... Is, typename... Ts>
+struct indexer<hera::index_sequence<Is...>, Ts...> : indexed<Is, Ts>...
+{};
+
+template<std::size_t I, typename T>
+static indexed<I, T> select(indexed<I, T>)
+{}
+} // namespace detail
+
+template<std::size_t I, typename... Ts>
+using nth_element_t = typename decltype(
+    select<I>(detail::indexer<hera::index_sequence_for<Ts...>, Ts...>{}))::type;
+} // namespace hera
---
+++ tests/nth_element.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch.hpp>
+
+#include "hera/concepts.hpp"
+#include "hera/nth_element.hpp"
+
+TEST_CASE("nth_element")
+{
+    static_assert(hera::same_as<int, hera::nth_element_t<0, int, bool, float>>);
+
+    static_assert(
+        hera::same_as<
+            float&&,
+            hera::nth_element_t<1, float, float&&, float&, const float&>>);
+}
