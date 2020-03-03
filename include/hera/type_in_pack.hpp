b891133f220d24d2f7eb191c9dea6b075d7f3b91
Frederik Engels <frederik.engels92@gmail.com>
2020-03-03 01:29:22 +0100

    added method to get index of type in pack if unique

---
+++ include/hera/type_in_pack.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "hera/algorithm/accumulate.hpp"
+#include "hera/algorithm/any_of.hpp"
+#include "hera/algorithm/find_if.hpp"
+#include "hera/container/integer_sequence.hpp"
+#include "hera/container/type_list.hpp"
+#include "hera/nth_element.hpp"
+
+namespace hera
+{
+
+namespace detail
+{
+template<typename U, typename... Ts>
+constexpr bool type_in_pack_impl()
+{
+    return decltype(hera::any_of(hera::type_list<Ts...>{}, [](auto el_type) {
+        using type = typename decltype(el_type)::type;
+        return std::is_same<type, U>{};
+    }))::value;
+}
+
+template<typename U, typename... Ts>
+constexpr bool type_in_pack_unique_impl()
+{
+    auto count = hera::accumulate(
+        hera::type_list<Ts...>{}, std::size_t{}, [](auto count, auto type) {
+            if constexpr (std::is_same_v<U, typename decltype(type)::type>)
+            {
+                return count + 1;
+            }
+            else
+            {
+                return count;
+            }
+        });
+
+    return count == 1; // 1 guarantees present and unique
+}
+} // namespace detail
+
+template<typename U, typename... Ts>
+concept type_in_pack = detail::type_in_pack_impl<U, Ts...>();
+
+template<typename U, typename... Ts>
+concept type_in_pack_unique = type_in_pack<U, Ts...>&&
+detail::type_in_pack_unique_impl<U, Ts...>();
+
+namespace detail
+{
+template<typename U, typename... Ts> // clang-format off
+    requires hera::type_in_pack_unique<U, Ts...>
+constexpr std::size_t type_in_pack_index_impl() // clang-format on
+{
+    return *hera::find_if(hera::type_list<Ts...>{}, [](auto el_type) {
+        using type = typename decltype(el_type)::type;
+
+        return std::is_same<U, type>{};
+    });
+}
+} // namespace detail
+
+template<typename U, typename... Ts>
+inline constexpr std::size_t
+    type_in_pack_index_v = detail::type_in_pack_index_impl<U, Ts...>();
+} // namespace hera
---
+++ tests/type_in_pack.cpp
@@ -0,0 +1,26 @@
+#include <catch2/catch.hpp>
+
+#include "hera/type_in_pack.hpp"
+
+TEST_CASE("type_in_pack")
+{
+    static_assert(hera::type_in_pack<int, int>);
+    static_assert(hera::type_in_pack<int, int&, int&&, int, const int>);
+    static_assert(
+        !hera::
+            type_in_pack<int, int&, const int, const int&, int&&, const int&&>);
+
+    SECTION("unique")
+    {
+        static_assert(hera::type_in_pack_unique<int, int>);
+        static_assert(!hera::type_in_pack_unique<int, int, float, int>);
+        static_assert(!hera::type_in_pack_unique<int>);
+    }
+
+    SECTION("index")
+    {
+        static_assert(hera::type_in_pack_index_v<int, int> == 0);
+        static_assert(
+            hera::type_in_pack_index_v<int, double, float, int, char> == 2);
+    }
+}
